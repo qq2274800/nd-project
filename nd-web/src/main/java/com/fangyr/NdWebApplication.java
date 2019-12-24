817d2ea54401775eb51c1eafe04471c12168b602
fangyurui <yurui.fang@ndmicro.com>
2019-12-24 10:53:26 +0800

    启动类位置优化

--- nd-web/src/main/java/com/fangyr/NdWebApplication.java
+++ nd-web/src/main/java/com/fangyr/NdWebApplication.java
@@ -1,12 +1,10 @@
-package com.fangyr.web;
+package com.fangyr;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.fangyr")
 @MapperScan("com.fangyr.dao.mapper")
 public class NdWebApplication {
 
